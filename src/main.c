2e0a3ebcd21198795c46ab61963b16dd3abd750c
Franklin <franklinbello99@gmail.com>
2019-10-15 01:54:22 -0400

    Modified the cell structure and added the checkneighbors functionality

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <time.h>
 
 // Dimensions of the Maze
 #define ROWS 10
@@ -9,33 +11,104 @@
 typedef uint8_t SMALL;
 
 typedef struct Cell{
-    SMALL r;  // row
-    SMALL c;  // col
-    bool n; // north 
-    bool s; // south 
-    bool e; // east
-    bool w; // west
+    SMALL row;      // row
+    SMALL col;      // col
+    bool walls[4];  // [0]top [1]right [2]bottom [3]left
+    bool visited;
 }Cell;
+
+void initGrid(Cell *grid){
+    
+    for(int i = 0; i < ROWS; i++){
+        for(int j = 0; j < COLS; j++){
+            Cell *ptr = grid + i*COLS + j;
+            ptr->row = i;
+            ptr->col = j;
+            ptr->walls[0] = true;
+            ptr->walls[1] = true;
+            ptr->walls[2] = true;
+            ptr->walls[3] = true;
+            ptr->visited = false;
+            printf("(%i, %i) ", ptr->row, ptr->col);
+        }
+        printf("\n");
+    }
+}
+
+Cell* getTopNeighbor(Cell *cell){
+    if(cell->row - 1 < 0){
+        return NULL;
+    } else {
+        return cell - COLS;
+    }
+}
+
+Cell* getRightNeighbor(Cell *cell){
+    if(cell->col + 1 > COLS - 1){
+        return NULL;
+    } else {
+        return cell + 1;
+    }
+}
+
+Cell* getBottomNeighbor(Cell *cell){
+    if(cell->row + 1 > ROWS - 1){
+        return NULL;
+    } else {
+        return cell + COLS;
+    }
+}
+
+Cell* getLeftNeighbor(Cell *cell){
+    if(cell->col - 1 < 0){
+        return NULL;
+    } else {
+        return cell - 1;
+    }
+}
+
+Cell* checkNeighbors(Cell *cell){
+    // Top neighbor
+    Cell *topNeighbor = getTopNeighbor(cell);
+    // Right neighbor
+    Cell *rightNeighbor = getRightNeighbor(cell);
+    // bottom neighbor
+    Cell *bottomNeighbor = getBottomNeighbor(cell);
+    // left neighbor
+    Cell *leftNeighbor = getLeftNeighbor(cell);
+
+    if(topNeighbor != NULL && !topNeighbor->visited){
+        printf("Top: (%i, %i)\n", topNeighbor->row, topNeighbor->col);
+    }
+    if(rightNeighbor != NULL && !rightNeighbor->visited){
+        printf("Right: (%i, %i)\n", rightNeighbor->row, rightNeighbor->col);
+    }
+    if(bottomNeighbor != NULL && !bottomNeighbor->visited){
+        printf("Bottom: (%i, %i)\n", bottomNeighbor->row, bottomNeighbor->col);
+    }
+    if(leftNeighbor != NULL && !leftNeighbor->visited){
+        printf("Left: (%i, %i)\n", leftNeighbor->row, leftNeighbor->col);
+    }
+
+    srand(0);
+
+    return NULL;
+}
 
 int main(void){
 
     Cell grid[ROWS][COLS];
+
+    // Set ups the pointer that will traverse the grid
     Cell *ptr = *grid;
 
-    for(int i = 0; i < ROWS; i++){
-        for(int j = 0; j < COLS; j++){
-            (ptr + i*COLS + j)->r = i;
-            (ptr + i*COLS + j)->c = j;
-            (ptr + i*COLS + j)->n = true;
-            (ptr + i*COLS + j)->s = true;
-            (ptr + i*COLS + j)->e = true;
-            (ptr + i*COLS + j)->w = true;
-            printf("(%i, %i) ", (ptr + i*COLS + j)->r, (ptr + i*COLS + j)->c);
-        }
-        printf("\n");
-    }
+    // Initialize grid
+    initGrid(ptr);
 
-    printf("Size of grid: %i bytes", sizeof(grid));
+    // The maze starts generating at (0,0)
+    ptr->visited = true;
+
+    ptr = checkNeighbors(ptr);
 
     return 0;
 }
