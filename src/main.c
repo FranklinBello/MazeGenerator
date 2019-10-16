5dd6405950d84ae217f72bba5302b874f40f6304
Franklin <franklinbello99@gmail.com>
2019-10-15 23:49:51 -0400

    Included linked list functions to store cell information dynamically

--- src/main.c
+++ src/main.c
@@ -10,12 +10,53 @@
 
 typedef uint8_t SMALL;
 
+// Cell structure to store the information of each cell
 typedef struct Cell{
     SMALL row;      // row
     SMALL col;      // col
     bool walls[4];  // [0]top [1]right [2]bottom [3]left
     bool visited;
 }Cell;
+
+// Node structure for linked list
+typedef struct Node
+{
+    Cell *cell;         // Pointer of cell
+    struct Node *next;  // Pointer to the next node
+}Node;
+
+// Adds an element at the beginning of the list
+void push(Node **head, Cell *cell){
+    // Allocate the node that will go at the beginning
+    Node *newHead = (Node*) malloc(sizeof(Node));
+    newHead->cell = cell;
+    newHead->next = *head;
+
+    // Move the head to point to the new node
+    *head = newHead;
+}
+
+Cell* getIndex(Node **head, SMALL index){
+    
+    Node *p = *head;
+    
+    for(SMALL i = 0; i < index; i++)
+        p = p->next;
+
+    return p->cell;
+}
+
+void freeList(Node **head){
+    Node *p = *head;
+    Node* tmp;
+
+    while (p != NULL)
+    {
+        tmp = p;
+        p = p->next;
+        free(tmp);
+    }
+}
 
 void initGrid(Cell *grid){
     
@@ -72,30 +113,50 @@ Cell* checkNeighbors(Cell *cell){
     Cell *topNeighbor = getTopNeighbor(cell);
     // Right neighbor
     Cell *rightNeighbor = getRightNeighbor(cell);
-    // bottom neighbor
+    // Bottom neighbor
     Cell *bottomNeighbor = getBottomNeighbor(cell);
-    // left neighbor
-    Cell *leftNeighbor = getLeftNeighbor(cell);
+    // Left neighbor
+    Cell *leftNeighbor = getLeftNeighbor(cell);    
+
+    Node *head = NULL;
+    SMALL n = 0;
 
     if(topNeighbor != NULL && !topNeighbor->visited){
         printf("Top: (%i, %i)\n", topNeighbor->row, topNeighbor->col);
+        push(&head, topNeighbor);
+        n++;
     }
     if(rightNeighbor != NULL && !rightNeighbor->visited){
         printf("Right: (%i, %i)\n", rightNeighbor->row, rightNeighbor->col);
+        push(&head, rightNeighbor);
+        n++;
     }
     if(bottomNeighbor != NULL && !bottomNeighbor->visited){
         printf("Bottom: (%i, %i)\n", bottomNeighbor->row, bottomNeighbor->col);
+        push(&head, bottomNeighbor);
+        n++;
     }
     if(leftNeighbor != NULL && !leftNeighbor->visited){
         printf("Left: (%i, %i)\n", leftNeighbor->row, leftNeighbor->col);
+        push(&head, leftNeighbor);
+        n++;
     }
 
-    srand(0);
+    if (n == 0)
+        return NULL;
 
-    return NULL;
+    int r = rand() % n;
+
+    Cell* neighbor = getIndex(&head, r);
+
+    freeList(&head);
+
+    return neighbor;
 }
 
 int main(void){
+
+    srand(time(NULL));
 
     Cell grid[ROWS][COLS];
 
@@ -108,7 +169,15 @@ int main(void){
     // The maze starts generating at (0,0)
     ptr->visited = true;
 
+    printf("First\n");
     ptr = checkNeighbors(ptr);
+    ptr->visited = true;
+    printf("Second\n");
+    ptr = checkNeighbors(ptr);
+    ptr->visited = true;
+    printf("Third\n");
+    ptr = checkNeighbors(ptr);
+    ptr->visited = true;
 
     return 0;
 }
